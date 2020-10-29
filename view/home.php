e8a9c57a32f873b0ee7730766cdbb09761d353b8
Luis mi <alexanderlara1996@gmail.com>
2020-10-28 21:33:43 -0500

    Guardando cambios

--- view/home.php
+++ view/home.php
@@ -11,14 +11,90 @@
 
         <!-- start header -->
         <?php include('./layout/header.php') ?>
-        <!-- end header -->        
+        <!-- end header -->
         <!-- start menu -->
         <?php include('./layout/menu.php') ?>
-        <!-- end menu --> 
+        <!-- end menu -->
         <!-- Content Wrapper. Contains page content -->
         <div class="content-wrapper" style="background-color: #FFFFFF;">
             <!-- Main content -->
             <section class="content">
+
+                <div class="row">
+                    <div class="col-lg-12 col-md-12">
+                        <div class="panel panel-default">
+                            <div class="panel-heading"> <i class="fa fa-user"></i> Bienvenido al Sistema</div>
+                            <div class="panel-body">
+                                <div class="alert alert-success">
+                                    {{ session('status') }}
+                                </div>
+                                <br><br>
+
+                                <div class="row">
+                                    <div class="col-lg-1 col-md-1 col-sm-12 col-xs-12"></div>
+                                    <div class="col-lg-10 col-md-10 col-sm-12 col-xs-12">
+                                        <div class="row">
+
+                                            <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+                                                <a href="{{ route('webmasters.index') }}">
+                                                    <div class="panel panel-default" style="border-style: dashed;border-width: 1px;border-color: #2A2A28;">
+                                                        <div class="panel-body" style="text-align: center;">
+                                                            <img src="{{ asset('images/webmaster.png') }}" alt="Vender" width="84">
+                                                            <p style="margin-top: 10px;font-size: 14pt;color:#C68907;">
+                                                                Usuarios
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                </a>
+                                            </div>
+                                            <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+                                                <a href="{{ route('ingenieros.index') }}">
+                                                    <div class="panel panel-default" style="border-style: dashed;border-width: 1px;border-color: #2A2A28;">
+                                                        <div class="panel-body" style="text-align: center;">
+                                                            <img src="{{ asset('images/tutores.png') }}" alt="Vender" width="84">
+                                                            <p style="margin-top: 10px;font-size: 14pt;color:#C68907;">
+                                                                Ingenieros
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                </a>
+                                            </div>
+                                            <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+                                                <a href="#">
+                                                    <div class="panel panel-default" style="border-style: dashed;border-width: 1px;border-color: #2A2A28;">
+                                                        <div class="panel-body" style="text-align: center;">
+                                                            <img src="{{ asset('images/pago.png') }}" alt="Vender" width="85">
+                                                            <p style="margin-top: 10px;font-size: 14pt;color:#C68907;">
+                                                                Cobros
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                </a>
+                                            </div>
+                                            <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+                                                <a href="#">
+                                                    <div class="panel panel-default" style="border-style: dashed;border-width: 1px;border-color: #2A2A28;">
+                                                        <div class="panel-body" style="text-align: center;">
+                                                            <img src="{{ asset('images/reporte.png') }}" alt="Vender" width="87">
+                                                            <p style="margin-top: 10px;font-size: 14pt;color:#C68907;">
+                                                                Reportes
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                </a>
+                                            </div>
+
+
+
+
+                                        </div>
+                                    </div>
+                                </div>
+                                <br><br>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
             </section>
             <!-- /.content -->
