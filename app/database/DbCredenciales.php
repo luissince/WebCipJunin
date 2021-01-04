ca36c503a55503631171f17575660e56bc6653ce
Rahamsis Correa marido de Lara <rahamsiscg.95@gmail.com>
2021-01-04 00:51:46 -0500

    Guardando cambios

--- app/database/DbCredenciales.php
+++ app/database/DbCredenciales.php
@@ -5,6 +5,6 @@
  */
 define("HOSTNAME", "localhost");// Nombre del host
 define("PORT", "1433");// Puerto de servidor
-define("DATABASE", "CIPJunin"); // Nombre de la base de datos
+define("DATABASE", "CIPJuninActual"); // Nombre de la base de datos
 define("USERNAME", "sa"); // Nombre del usuario
 define("PASSWORD", "Qz0966lb"); // Nombre de la constraseña
