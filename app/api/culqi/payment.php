f152b221677295c27dbf19f5f2adc92fe0d00f87
Luis mi <alexanderlara1996@gmail.com>
2021-07-09 08:06:28 -0500

    Guardando cambios

--- app/api/culqi/payment.php
+++ app/api/culqi/payment.php
@@ -1,5 +1,10 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('Content-Type: application/json; charset=UTF-8');
+date_default_timezone_set('America/Lima');
 
 // $body = json_decode(file_get_contents("php://input"), true);
 // $postdata = http_build_query(
@@ -25,12 +30,16 @@
 // $manage = json_decode($data);
 // print json_encode($manage);
 
+$body = json_decode(file_get_contents("php://input"), true);
+$fechaactual = new DateTime('now');
+$yearinicio = substr($fechaactual->format("Y"), 0, 2);
+
 $data =  array(
-    'card_number' => "5111111111111118",
-    'cvv' => "039",
-    'expiration_month' => "06",
-    'expiration_year' => "2025",
-    'email' => "richard@piedpiper.com"
+    'card_number' => $body["card_number"],
+    'cvv' => $body["cvv"],
+    'expiration_month' => $body["expiration_month"],
+    'expiration_year' => $yearinicio . $body["expiration_year"],
+    'email' => $body["email"],
 );
 
 $data_string = json_encode($data);
@@ -60,10 +69,32 @@ curl_close($curl);
 if ($http_code == 201) {
     $result = (object)json_decode($resp);
 
+
+    /*
+10 = 10 00
+100 = 100 00
+*/
+    // Creando Cargo a una tarjeta
+
+    // $porcetaje = 4.20 / 100; //0.042
+    // $monto = floatval($body["monto"]) + 2;
+    // $igv = 18;
+    // $comision = $monto * $porcetaje; //4.20
+    // $igv = $comision * ($igv / 100); //0.756
+    // $total = round($monto + $comision + $igv, 0, PHP_ROUND_HALF_UP) * 100;
+    $total = floatval($body["monto"]) * 100;
+
+    /*
+240 s
+240.46 d
+0.46
+*/
+
+
     $data =  array(
-        "amount" => "10000",
+        "amount" => $total,
         "currency_code" => "PEN",
-        "email" => "perrisd@piedpiper.com",
+        "email" => $body["email"],
         "source_id" =>  $result->id
     );
 
@@ -91,13 +122,31 @@ if ($http_code == 201) {
     curl_close($curl);
 
     if ($http_code == 201) {
-        print "bien";
+        print json_encode(
+            array(
+                "estado" => 1,
+                "data" => "Se registro correctamente el pago.",
+                "result" => (object)json_decode($resp)
+            )
+        );
     } else {
-        print "Error al crear cobro, intente nuevamente porfavor.";
+        print json_encode(
+            array(
+                "estado" => 0,
+                "message" => "Error en procesar el pago, intente nuevamente en un par de minutos.",
+                "error" => (object)json_decode($resp)
+            )
+        );
         // $result = (object)json_decode($resp);
         // var_dump($result);
     }
-
 } else {
-    print "Error al crear el token id, intente nuevamente porfavor.";
+    $result = (object)json_decode($resp);
+    print json_encode(
+        array(
+            "estado" => 0,
+            "message" => "Error al crear el token id, intente nuevamente porfavor.",
+            "error" => $result
+        )
+    );
 }
