b789aaace635035ff833b0de1107ee5e18557394
Luis mi <alexanderlara1996@gmail.com>
2022-06-23 17:04:44 -0500

    Guardando cambios

--- app/src/Tools.php
+++ app/src/Tools.php
@@ -12,14 +12,14 @@ class Tools
     {
         $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('AES-256-CBC'));
         $encrypted = openssl_encrypt($encrypt, 'AES-256-CBC', $key, 0, $iv);
-        return rtrim(strtr(base64_encode($encrypted . "::" . $iv), '+/', '-_'), '=');
+        return strtr(base64_encode($encrypted . "::" . $iv), '+/=', '._-');
     }
 
     public static  function my_decrypt($encrypted, $key)
     {
         $explote = explode(
             '::',
-            base64_decode($encrypted),
+            base64_decode(strtr($encrypted, '._-', '+/=')),
             2
         );
 
