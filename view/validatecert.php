04576910200d735ec5e200517f2a6ed17c0ae3ad
Luis mi <alexanderlara1996@gmail.com>
2022-06-23 14:59:31 -0500

    Guardando cambios

--- view/validatecert.php
+++ view/validatecert.php
@@ -34,7 +34,7 @@
                 <div class="row">
                     <div class="col-md-3">
                         <!-- Profile Image -->
-                        <div class="box box-primary">
+                        <div class="box box-danger">
                             <div class="box-body box-profile">
                                 <img class="profile-user-img img-responsive img-circle" src="images/noimage.jpg" alt="User profile picture" id="lblImagen">
 
