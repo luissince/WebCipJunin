e2d5cad118d0d0d3389fc6752454ef1fbb5bd8bc
Ruber Flores <ruberfc@gmail.com>
2022-07-18 17:44:15 -0500

    Guardando cambios

--- view/mensaje.php
+++ view/mensaje.php
@@ -35,7 +35,7 @@ if (!isset($_SESSION['IdUsuario'])) {
                                     </button>
                                     <h4 class="modal-title" id="titleModal">
                                         <i class="fa fa-address-card">
-                                        </i> Nueva Oferta Laboral
+                                        </i> Nueva Mensaje
                                     </h4>
                                 </div>
                                 <div class="modal-body">
@@ -147,7 +147,7 @@ if (!isset($_SESSION['IdUsuario'])) {
                                     </button>
                                     <h4 class="modal-title">
                                         <i class="fa fa-address-card">
-                                        </i> Eliminar oferta laboral
+                                        </i> Eliminar Mensaje
                                     </h4>
                                 </div>
                                 <div class="modal-body">
@@ -175,17 +175,17 @@ if (!isset($_SESSION['IdUsuario'])) {
                 <div class="content-wrapper" style="background-color: #FFFFFF;">
                     <!-- Main content -->
                     <section class="content-header">
-                        <h3 class="no-margin"> Oferta Laboral <small> Lista </small> </h3>
+                        <h3 class="no-margin"> Mensajes (App) <small> Lista </small> </h3>
                     </section>
 
                     <section class="content">
 
-                        <div class="row">
+                        <div class="row">                
                             <div class="col-lg-2 col-md-3 col-sm-12 col-xs-12">
                                 <label>Nueva oferta.</label>
                                 <div class="form-group">
                                     <button type="button" class="btn btn-success" id="btnNuevo">
-                                        <i class="fa fa-plus"></i> Agregar oferta
+                                        <i class="fa fa-paper-plane"></i> Crear Mensaje
                                     </button>
                                 </div>
                             </div>
@@ -277,345 +277,6 @@ if (!isset($_SESSION['IdUsuario'])) {
                 let tbTable = $("#tbTable");
 
                 let idUsuario = <?= $_SESSION['IdUsuario'] ?>;
-
-                let idEmpleo = '';
-
-                $(document).ready(function() {
-
-                    loadInitEmpleos();
-
-                    $("#btnIzquierda").click(function() {
-                        if (!state) {
-                            if (paginacion > 1) {
-                                paginacion--;
-                                onEventPaginacion();
-                            }
-                        }
-                    });
-
-                    $("#btnDerecha").click(function() {
-                        if (!state) {
-                            if (paginacion < totalPaginacion) {
-                                paginacion++;
-                                onEventPaginacion();
-                            }
-                        }
-                    });
-
-                    $("#btnactualizar").click(function() {
-                        loadInitEmpleos()
-                    });
-
-                    $("#buscar").on("keyup", function(event) {
-                        if (event.keyCode === 13) {
-                            paginacion = 1;
-                            loadTableEmpleos($("#buscar").val());
-                            opcion = 1;
-                        }
-                    });
-
-                    $("#btnNuevo").click(function() {
-                        openModalEmpleo('');
-                    })
-
-                    $("#btnCloseModal").click(function() {
-                        $("#mdEmpleo").modal("hide");
-                        clearModal();
-                    });
-
-                    $("#cancel-modal").click(function() {
-                        $("#mdEmpleo").modal("hide");
-                        clearModal();
-                    });
-
-                    $("#btnAceptar").click(function() {
-                        onSaveEmpleo(
-                            $("#txtTitulo").val(),
-                            $("#txtDescripcion").val(),
-                            $("#txtEmpresa").val(),
-                            $("#txtCelular").val(),
-                            $("#txtTelefono").val(),
-                            $("#txtCorreo").val(),
-                            $("#txtDireccion").val());
-                    });
-
-
-                });
-
-                function closeModalDelete() {
-                    $("#mdDeleteEmpleo ").modal("hide");
-                }
-
-                function updateEmpleoData(id) {
-                    $.ajax({
-                        url: "../app/controller/EmpleoController.php",
-                        method: "GET",
-                        data: {
-                            "type": "id",
-                            "idEmpleo": id
-                        },
-                        beforeSend: function() {
-                            idEmpleo = 0;
-                        },
-                        success: function(result) {
-                            idEmpleo = id;
-                            $("#txtTitulo").val(result.object.Titulo);
-                            $("#txtDescripcion").val(result.object.Descripcion);
-                            $("#txtEmpresa").val(result.object.Empresa);
-                            $("#txtCelular").val(result.object.Celular);
-                            $("#txtTelefono").val(result.object.Telefono);
-                            $("#txtCorreo").val(result.object.Correo);
-                            $("#txtDireccion").val(result.object.Direccion);
-
-                            document.getElementById("cbEstado").checked = result.object.Estado == 1 ? true : false;
-
-                            tools.AlertInfo("Empleo", "Se cargo correctamente los datos.");
-                        },
-                        error: function() {
-                            tools.AlertError("Empleo", error.responseText);
-                        }
-                    });
-                }
-
-                function openModalEmpleo(idEmpleo) {
-
-                    $("#titleModal").html('')
-
-                    if (idEmpleo === '') {
-
-                        $("#titleModal").html('<i class="fa fa-address-card"></i> Nueva Oferta Laboral')
-
-                        $("#mdEmpleo").modal("show");
-                    } else {
-
-                        $("#titleModal").html('<i class="fa fa-address-card"></i> Editar Oferta Laboral')
-
-                        $("#mdEmpleo").modal("show");
-
-                        updateEmpleoData(idEmpleo);
-
-                    }
-                }
-
-                function onEventPaginacion() {
-                    switch (opcion) {
-                        case 0:
-                            loadTableEmpleos("");
-                            break;
-                        case 1:
-                            loadTableEmpleos($("#buscar").val());
-                            break;
-                    }
-                }
-
-                function loadInitEmpleos() {
-                    if (!state) {
-                        paginacion = 1;
-                        loadTableEmpleos("");
-                        opcion = 0;
-                    }
-                }
-
-                function loadTableEmpleos(text) {
-                    $.ajax({
-                        url: "../app/controller/EmpleoController.php",
-                        method: "GET",
-                        data: {
-                            "type": "alldata",
-                            "text": text,
-                            "posicionPagina": ((paginacion - 1) * filasPorPagina),
-                            "filasPorPagina": filasPorPagina
-                        },
-                        beforeSend: function() {
-                            tbTable.empty();
-                            tbTable.append(
-                                '<tr class="text-center"><td colspan="8"><img src="./images/spiner.gif"/><p>Cargando información.</p></td></tr>'
-                            );
-                            state = true;
-                            totalPaginacion = 0;
-                        },
-                        success: function(result) {
-                            tbTable.empty();
-                            if (result.empleos.length == 0) {
-                                tbTable.append(
-                                    '<tr class="text-center"><td colspan="8"><p>No hay datos para mostrar</p></td></tr>'
-                                );
-                                $("#lblPaginaActual").html(0);
-                                $("#lblPaginaSiguiente").html(0);
-                                state = false;
-                            } else {
-                                for (let empleo of result.empleos) {
-
-                                    let btnUpdate = `<button class="btn btn-warning btn-xs" onclick="openModalEmpleo(${empleo.idEmpleo} )"><i class="fa fa-edit" style="font-size:25px;"></i></button>`;
-                                    let btnDelete = `<button class="btn btn-danger btn-xs" onclick="deleteEmpleo(${empleo.idEmpleo})"><i class="fa fa-trash" style="font-size:25px;"></i></button>`
-
-                                    let estado = empleo.Estado == 1 ? '<span class="badge btn-info">ACTIVO</span>' : '<span class="badge btn-danger">INACTIVO</span>'
-
-                                    tbTable.append('<tr>' +
-                                        '<td class="text-center text-primary">' + empleo.Id + '</td>' +
-                                        '<td>' + empleo.Titulo + '</td>' +
-                                        '<td>' + empleo.Fecha + '<br/>' + tools.getTimeForma(empleo.Hora, true) + '</td>' +
-                                        '<td>' + empleo.Celular + '</td>' +
-                                        '<td>' + empleo.Empresa + '</td>' +
-                                        '<td>' + estado + '</td>' +
-                                        '<td class="text-center">' + btnUpdate + '' + '</td>' +
-                                        '<td class="text-center">' + btnDelete + '</td>' +
-                                        '</tr>');
-                                }
-                                totalPaginacion = parseInt(Math.ceil((parseFloat(result.total) / parseInt(
-                                    filasPorPagina))));
-                                $("#lblPaginaActual").html(paginacion);
-                                $("#lblPaginaSiguiente").html(totalPaginacion);
-                                state = false;
-                            }
-                        },
-                        error: function(error) {
-                            console.log(error.responseText)
-                            tbTable.empty();
-                            tbTable.append(
-                                '<tr class="text-center"><td colspan="8"><p>Se produjo un error, intente nuevamente.</p></td></tr>'
-                            );
-                            $("#lblPaginaActual").html(0);
-                            $("#lblPaginaSiguiente").html(0);
-                            state = false;
-                        }
-                    });
-                }
-
-                function onSaveEmpleo(titulo, descripcion, empresa, celular, telefono, correo, direccion) {
-                    if (titulo === '') {
-                        tools.AlertWarning('Empleo', "Ingrese el titulo");
-                        $("#txtTitulo").focus();
-                        return;
-                    }
-
-                    if (descripcion === '') {
-                        tools.AlertWarning('Empleo', "Ingrese la descripción");
-                        $("#txtDescripcion").focus();
-                        return;
-                    }
-
-                    if (empresa === '') {
-                        tools.AlertWarning('Empleo', "Ingrese el nombre de la empresa");
-                        $("#txtEmpresa").focus();
-                        return;
-                    }
-                    if (celular === '') {
-                        tools.AlertWarning('Empleo', "Ingrese el número de celular");
-                        $("#txtCelular").focus();
-                        return;
-                    }
-
-                    if (idEmpleo === '') {
-
-                        $.ajax({
-                            url: "../app/controller/EmpleoController.php",
-                            method: "POST",
-                            data: {
-                                "type": "insert",
-                                "Titulo": titulo.trim(),
-                                "Descripcion": descripcion.trim(),
-                                "Empresa": empresa.trim(),
-                                "Celular": celular.trim(),
-
-                                "Telefono": telefono.trim(),
-                                "Correo": correo.trim(),
-                                "Direccion": direccion.trim(),
-                                "Estado": $('#cbEstado').is(':checked'),
-                                "Tipo": 1,
-                                "idUsuario": idUsuario
-
-                            },
-                            beforeSend: function() {
-                                tools.AlertInfo("Empleo", "Procesando información.");
-                            },
-                            success: function(result) {
-                                $("#mdEmpleo").modal("hide");
-                                tools.AlertSuccess("Empleo", "Se registro correctamente.");
-                                clearModal();
-                            },
-                            error: function(error) {
-                                tools.AlertError("Empleo", "Error fatal: Comuniquese con el administrador del sistema.");
-                            }
-                        });
-                    } else {
-
-                        $.ajax({
-                            url: "../app/controller/EmpleoController.php",
-                            method: "POST",
-                            data: {
-                                "type": "update",
-                                "Titulo": titulo.trim(),
-                                "Descripcion": descripcion.trim(),
-                                "Empresa": empresa.trim(),
-                                "Celular": celular.trim(),
-
-                                "Telefono": telefono.trim(),
-                                "Correo": correo.trim(),
-                                "Direccion": direccion.trim(),
-                                "Estado": $('#cbEstado').is(':checked'),
-                                "Tipo": 1,
-                                "idUsuario": idUsuario,
-                                "idEmpleo": idEmpleo
-
-                            },
-                            beforeSend: function() {
-                                tools.AlertInfo("Empleo", "Procesando información.");
-                            },
-                            success: function(result) {
-                                $("#mdEmpleo").modal("hide");
-                                tools.AlertSuccess("Empleo", "Se actualizó correctamente.");
-                                clearModal();
-                            },
-                            error: function(error) {
-                                tools.AlertError("Empleo", "Error fatal: Comuniquese con el administrador del sistema.");
-                            }
-                        });
-                    }
-                }
-
-                function deleteEmpleo(id) {
-                    $("#mdDeleteEmpleo ").modal("show");
-
-                    let idEmpleo = id;
-
-                    $("#btnDeleteEmpleo").unbind();
-
-                    $("#btnDeleteEmpleo").bind("click", function() {
-
-                        $.ajax({
-                            url: "../app/controller/EmpleoController.php",
-                            method: "POST",
-                            data: {
-                                "type": "delete",
-                                "idEmpleo": idEmpleo,
-                            },
-                            beforeSend: function() {
-                                $("#mdDeleteEmpleo").modal("hide");
-                                tools.ModalAlertInfo("Empleo", "Procesando petición..");
-                            },
-                            success: function(result) {
-                                tools.ModalAlertSuccess("Empleo", result.message);
-                                loadInitEmpleos()
-                            },
-                            error: function(error) {
-                                tools.AlertError("Empleo", "Error fatal: Comuniquese con el administrador del sistema.");
-                            }
-                        });
-                    })
-                }
-
-                function clearModal() {
-                    $("#txtTitulo").val('');
-                    $("#txtDescripcion").val('');
-                    $("#txtEmpresa").val('');
-                    $("#txtCelular").val('');
-                    $("#txtTelefono").val('');
-                    $("#txtCorreo").val('');
-                    $("#txtDireccion").val('');
-                    document.getElementById("cbEstado").checked = true;
-                    idEmpleo = '';
-                }
             </script>
         </body>
 
